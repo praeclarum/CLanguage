3868e1d8bba96bfcb9bd33cb7fd9f2ae93555310
Frank A. Krueger <fak@praeclarum.org>
2018-12-12 17:38:16 -0800

    Better dark error color

--- Editor/Mac/Theme.cs
+++ Editor/Mac/Theme.cs
@@ -76,7 +76,7 @@ namespace CLanguage.Editor
             }.Dictionary;
 
             BackgroundColor = !isDark ? Rgb (0xFF, 0xFF, 0xFF) : Rgb (41, 42, 47);
-            ErrorBubbleBackgroundColor = !isDark ? Rgb (0xC5, 0, 0xB) : Rgb (0xC5 * 2 / 4, 0, 0xB);
+            ErrorBubbleBackgroundColor = !isDark ? Rgb (0xC5, 0, 0xB) : Rgb (0xC5 * 5 / 8, 0, 0xB);
             ErrorBubbleTextAttributes = new NativeStringAttributes {
                 ForegroundColor = Gray (255).ColorWithAlphaComponent (0.9375f),
                 Font = NativeFont.BoldSystemFontOfSize (NativeFont.SystemFontSize),
