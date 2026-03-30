9fe936f684a2729009d2d9438a72e63b133a0b92
Frank A. Krueger <fak@praeclarum.org>
2026-03-30 10:35:00 -0700

    Add jay source code

--- CLanguage/prebuild.ps1
+++
@@ -1,5 +0,0 @@
-$oldPWD = Get-Location
-Set-Location Parser
-$skeleton = Get-Content -Raw "../../Lib/skeleton.cs"
-$skeleton | &"../../Lib/jay.exe" -vc CParser.jay > CParser.cs
-Set-Location $oldPWD
--- CLanguage/prebuild.sh
+++ CLanguage/prebuild.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cd Parser && ../../Lib/jay -vc CParser.jay < ../../Lib/skeleton.cs > CParser.cs
-
+set -e
+cd ../Jay && gcc -DSKEL_DIRECTORY='"."' -o jay src/closure.c src/error.c src/lalr.c src/lr0.c src/main.c src/mkpar.c src/output.c src/reader.c src/symtab.c src/verbose.c src/warshall.c
+cd ../CLanguage/Parser && ../../Jay/jay -vc CParser.jay < ../../Jay/skeleton.cs > CParser.cs
